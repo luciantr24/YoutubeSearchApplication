2395f6f1c1d2ce29d02fe44ec8bca43d11d3ffe8
luciantr24 <47867053+luciantr24@users.noreply.github.com>
2020-01-12 13:58:20 +0200

    Update Video.cs

--- Video.cs
+++ Video.cs
@@ -9,20 +9,9 @@ namespace YoutubeSearchApplication
 {
     class Video
     {
-
-
         public string Title { get; set; }
         public string Author { get; set; }
         public string Url { get; set; }
         public Image Thumbnail { get; set; }
-
-
-
-
-
-
-
-
-
     }
 }
